3c9bf27b0e5df62f40b64f3ea5b455e2114663d0
Marcus Österberg <marcus.oesterberg@internations.org>
2017-01-15 21:11:14 +0100

    Write json to document

--- main.go
+++ main.go
@@ -1,24 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 )
 
-func prettyfyRequest(req *http.Request) {
-	requestDump, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(string(requestDump))
+type Payload struct {
+	Payload Data
+}
+
+type Data struct {
+	Book, Podcast map[string]string
 }
 
 func handler(write http.ResponseWriter, request *http.Request) {
-	prettyfyRequest(request)
+	requestDump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(requestDump))
+
+	response, err := getJSONResponse()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(write, string(response))
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe("localhost:1337", nil)
+}
+
+func getJSONResponse() ([]byte, error) {
+	books := make(map[string]string)
+
+	books["Ego is the enemy"] = "Get rid of your ego and achieve greater things"
+	books["Manage Your Day-to-Day"] = "Great book for building daily routines"
+
+	podcasts := make(map[string]string)
+
+	podcasts["seanwes podcast"] = "How to pursuit and make a living of your passion"
+	podcasts["Changelog"] = "Interviews with the most influential people from the open source community"
+
+	data := Data{books, podcasts}
+	payload := Payload{data}
+
+	return json.MarshalIndent(payload, "", " ")
 }
